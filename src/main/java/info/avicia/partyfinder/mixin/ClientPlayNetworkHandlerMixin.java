23c3f6e3eaf330fc37cc797db192f3dd67a5c5fc
mark <mark.henein9616@gmail.com>
2026-06-18 15:28:49 -0400

    remove onChatMessage interceptor

--- src/main/java/info/avicia/partyfinder/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/info/avicia/partyfinder/mixin/ClientPlayNetworkHandlerMixin.java
@@ -2,7 +2,6 @@ package info.avicia.partyfinder.mixin;
 
 import info.avicia.partyfinder.PartyFinderMod;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
-import net.minecraft.network.packet.s2c.play.ChatMessageS2CPacket;
 import net.minecraft.network.packet.s2c.play.GameMessageS2CPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -23,23 +22,6 @@ public class ClientPlayNetworkHandlerMixin {
             }
         } catch (Exception e) {
             PartyFinderMod.LOGGER.error("Error handling game message in mixin", e);
-        }
-    }
-
-    @Inject(method = "onChatMessage", at = @At("HEAD"))
-    private void onOnChatMessage(ChatMessageS2CPacket packet, CallbackInfo ci) {
-        try {
-            String text = null;
-            if (packet.unsignedContent() != null) {
-                text = packet.unsignedContent().getString();
-            } else if (packet.body() != null) {
-                text = packet.body().content();
-            }
-            if (text != null && PartyFinderMod.getInstance() != null && PartyFinderMod.getInstance().getChatDetector() != null) {
-                PartyFinderMod.getInstance().getChatDetector().onChatMessage(text);
-            }
-        } catch (Exception e) {
-            PartyFinderMod.LOGGER.error("Error handling chat message in mixin", e);
         }
     }
 }
